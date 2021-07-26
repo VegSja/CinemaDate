50635a560ed8b496bbb637f5cf3e779919446026
Vegard <vgsjaavik@gmail.com>
2021-07-26 16:26:07 +0000

    Liked movies request

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,4 +1,5 @@
 const UserModel = require('../models/userModel')
+const MovieModel = require('../models/movieModel')
 
 const getLikedMovies = async(req, res) => {
 	const user = await UserModel.findOne({email: req.user})
@@ -17,6 +18,33 @@ const getLikedMovies = async(req, res) => {
 		})
 }
 
+const getUnratedMovies = async(req, res) => {
+	let ratedMovies = []
+	const user = await UserModel.findOne({email: req.user})
+		.populate("liked_movies")
+		.populate("disliked_movies")
+		.then(docs => {
+			ratedMovies.push(...docs.liked_movies)
+			ratedMovies.push(...docs.disliked_movies)
+			ratedMovies = ratedMovies.map(movie => movie.name)
+			return MovieModel.getAllMovies()
+		})
+		.then(all_movies => {
+			const filtered_array = all_movies.filter((movie) => !ratedMovies.includes(movie.name))
+			res.status(200).json({
+				success: true,
+				data: filtered_array
+			})
+		})
+		.catch(err => {
+			console.log(err)
+			res.status(400).json({
+				success: false,
+				message: error
+			})
+		})
+}
+
 const addLikedMovie = async(req, res) => {
 	const movieId = req.body.movieId
 	const user = await UserModel.findOne({email: req.user})
@@ -25,7 +53,6 @@ const addLikedMovie = async(req, res) => {
 			return doc.save()
 		})
 		.then(doc => {
-			console.log("Successfully saved to server", doc)
 			res.status(200).json({
 				success: true,
 				data: doc
@@ -41,5 +68,6 @@ const addLikedMovie = async(req, res) => {
 }
 module.exports = {
 	getLikedMovies,
+	getUnratedMovies,
 	addLikedMovie
 }
--- server/routes/user.js
+++ server/routes/user.js
@@ -2,11 +2,15 @@ const express = require('express')
 const router = express.Router()
 const {
 	getLikedMovies,
+	getUnratedMovies,
 	addLikedMovie
 } = require('../controllers/user')
 
 router.route('/liked/movies')
 	.get(getLikedMovies)
 	.post(addLikedMovie)
+
+router.route('/unrated/movies')
+	.get(getUnratedMovies)
 
 module.exports = router
